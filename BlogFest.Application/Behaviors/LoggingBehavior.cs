ecf4e9ab59c3c71e6c83b88e3bdeb1a2d5dc5111
kelace <matetyson@gmail.com>
2024-03-23 10:40:58 +0200

    fix(LoggingBehavior): added generic type to logger

--- BlogFest.Application/Behaviors/LoggingBehavior.cs
+++ BlogFest.Application/Behaviors/LoggingBehavior.cs
@@ -5,7 +5,11 @@ namespace BlogFest.Application.Behaviors
 {
     public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
-        private readonly ILogger _logger;
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             _logger.LogInformation("Begin command" + nameof(TRequest));
